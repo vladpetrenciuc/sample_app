40ae4370e7c89465dcbccd3bbae4597056e2e5f9
Vlad-Adrian Petrenciuc <vlad.petrenciuc@3pillarglobal.com>
2019-08-01 14:43:16 +0300

    Added account activation

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,7 +95,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = '<your heroku app>.herokuapp.com'
+  host = 'sample_app.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
