f72c8f075d1ab5bde1e5a9e2d73985eb38a335b5
Vlad-Adrian Petrenciuc <vlad.petrenciuc@3pillarglobal.com>
2019-08-01 14:28:11 +0300

    Added account activation

--- app/models/user.rb
+++ app/models/user.rb
@@ -54,12 +54,12 @@ class User < ApplicationRecord
 
     # Converts email to all lower-case.
     def downcase_email
-      self.email = email.downcase
-    end
-
-    # Creates and assigns the activation token and digest.
-    def create_activation_digest
-      self.activation_token  = User.new_token
-      self.activation_digest = User.digest(activation_token)
-    end
+        self.email = email.downcase
+      end
+  
+      # Creates and assigns the activation token and digest.
+      def create_activation_digest
+        self.activation_token  = User.new_token
+        self.activation_digest = User.digest(activation_token)
+      end
 end
